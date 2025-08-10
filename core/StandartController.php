1891cf5d3675718e4b98a82f5baeae06a79bb79c
Danila Cockin <tokkkarinzedebug@yandex.ru>
2025-08-11 00:00:57 +0300

    try to fix cors

--- core/StandartController.php
+++ core/StandartController.php
@@ -14,12 +14,26 @@ class StandartController
 			http_response_code(415);
 			die(json_encode(['error' => 'Unsupported Media Type']));
 		}
+		$allowedOrigins = [
+			"http://localhost:5173",
+			"https://shirma.fun"
+		];
+
+		$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+		if (in_array($origin, $allowedOrigins)) {
+			header("Access-Control-Allow-Origin: $origin");
+		} else {
+			header("Access-Control-Allow-Origin: *");
+		}
+
+		header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+		header('Access-Control-Allow-Headers: Content-Type, Authorization');
 		header('Content-Type: application/json');
-		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Methods: GET, POST');
-		header('Access-Control-Allow-Credentials: true');
-		header("Access-Control-Allow-Headers: Content-Type, Authorization");
-		header("Access-Control-Allow-Credentials: true"); 
+
+		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+			exit(0);
+		}
 	}
 }
 
