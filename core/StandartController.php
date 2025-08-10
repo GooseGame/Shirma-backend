27d4dc41502e9f12e6d2427828248c86ff8adbbe
Danila Cockin <tokkkarinzedebug@yandex.ru>
2025-08-10 23:39:29 +0300

    try to fix cors

--- core/StandartController.php
+++ core/StandartController.php
@@ -18,6 +18,8 @@ class StandartController
 		header('Access-Control-Allow-Origin: *');
 		header('Access-Control-Allow-Methods: GET, POST');
 		header('Access-Control-Allow-Credentials: true');
+		header("Access-Control-Allow-Headers: Content-Type, Authorization");
+		header("Access-Control-Allow-Credentials: true"); 
 	}
 }
 
