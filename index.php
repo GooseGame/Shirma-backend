84fef208c754a8634aa474b415e0146cb7a0d320
Danila Cockin <tokkkarinzedebug@yandex.ru>
2025-08-12 15:58:25 +0300

    fix index.php

--- index.php
+++ index.php
@@ -5,43 +5,43 @@ require __DIR__ . '/vendor/autoload.php';
 $request = $_SERVER['REQUEST_URI'];
 switch (parse_url($request, PHP_URL_PATH)) {
     case '/login/google':
-        $controller = new App\Controllers\LoginController();
+        $controller = new app\Controllers\LoginController();
         $controller->google();
         break;
 	case '/logout':
-		$controller = new App\Controllers\AuthController();
+		$controller = new app\Controllers\AuthController();
 		$controller->logout();
 		break;
 	case '/refresh':
-		$controller = new App\Controllers\AuthController();
+		$controller = new app\Controllers\AuthController();
 		$controller->refresh();
 		break;
 	case '/user/get':
-		$controller = new App\Controllers\LoginController();
+		$controller = new app\Controllers\LoginController();
         $controller->getUser();
 		break;
 	case '/user/changeName':
-		$controller = new App\Controllers\LoginController();
+		$controller = new app\Controllers\LoginController();
         $controller->changeName();
 		break;
 	case '/user/delete':
-		$controller = new App\Controllers\LoginController();
+		$controller = new app\Controllers\LoginController();
         $controller->deleteAccount();
 		break;
     case '/characters/get':
-        $controller = new App\Controllers\CharactersController();
+        $controller = new app\Controllers\CharactersController();
         $controller->get();
         break;
 	case '/characters/save':
-        $controller = new App\Controllers\CharactersController();
+        $controller = new app\Controllers\CharactersController();
         $controller->save();
         break;
 	case '/presets/get':
-        $controller = new App\Controllers\PresetsController();
+        $controller = new app\Controllers\PresetsController();
         $controller->get();
         break;
 	case '/presets/save':
-        $controller = new App\Controllers\PresetsController();
+        $controller = new app\Controllers\PresetsController();
         $controller->save();
         break;
     default:
