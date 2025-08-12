1352af23a577700cb65fad2725fef26e5f30463a
Danila Cockin <tokkkarinzedebug@yandex.ru>
2025-08-12 18:47:16 +0300

    try to move headers on top

--- core/StandartController.php
+++ core/StandartController.php
@@ -14,26 +14,7 @@ class StandartController
 			http_response_code(415);
 			die(json_encode(['error' => 'Unsupported Media Type']));
 		}
-		$allowedOrigins = [
-			"http://localhost:5173",
-			"https://shirma.fun"
-		];
 
-		$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-		if (in_array($origin, $allowedOrigins)) {
-			header("Access-Control-Allow-Origin: $origin");
-		} else {
-			header("Access-Control-Allow-Origin: *");
-		}
-
-		header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-		header('Access-Control-Allow-Headers: Content-Type, Authorization');
-		header('Content-Type: application/json');
-
-		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-			exit(0);
-		}
 		$this->db = createPDO();
 		} catch (\Exception $e) {
 			echo $e->getMessage();
--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+$allowedOrigins = [
+    "http://localhost:5173",
+    "https://shirma.fun"
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+} else {
+    header("Access-Control-Allow-Origin: *");
+}
+
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 // Маршрутизация
 $request = $_SERVER['REQUEST_URI'];
 switch (parse_url($request, PHP_URL_PATH)) {
